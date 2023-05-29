4e5cd45f1e175171c9b4157ef434f574aa7811cb
Ariunbayar Tsedev <tsedev.ariunbayar@gmail.com>
2023-05-30 02:02:49 +0800

    pep corrected

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import Response, status, viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
 from .models import Task
 from .serializers import TaskSerializer
